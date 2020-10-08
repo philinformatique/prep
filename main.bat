8b94fc503c6cbf19f04d0533e3c2cf813ac82dfd
philinformatique <phil5_8@hotmail.com>
2020-10-08 17:47:19 -0400

    Update main.bat

--- main.bat
+++ main.bat
@@ -1,2 +1,3 @@
-curl -o script2.bat https://raw.githubusercontent.com/philinformatique/prep/main/script.bat
-script.bat
+md %windir%\PI
+curl -o %windir%\PI\script2.bat https://raw.githubusercontent.com/philinformatique/prep/main/script.bat
+%windir%\PI\script2.bat

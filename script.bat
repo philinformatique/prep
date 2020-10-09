cd5046ea1648e684a0001363ba0dbe2d64ca2a0b
philinformatique <phil5_8@hotmail.com>
2020-10-09 08:29:10 -0400

    Update script.bat

--- script.bat
+++ script.bat
@@ -33,7 +33,7 @@ shutdown /r
 set /p reboot=Votre PC redemarrera pensez a mettre le HD seul en boot...
 
 :diskmanagement
-echo Hostname : %ComputerName%
+echo Nom du PC : %ComputerName%
 echo.
 wmic bios get serialnumber>>%mypath:~0,-1%\%ComputerName%.txt
 

dc739bd1404bb98df33587ae80d04a4addc207fe
philinformatique <phil5_8@hotmail.com>
2020-10-16 11:13:53 -0400

    Update main.bat

--- main.bat
+++ main.bat
@@ -1,3 +1,9 @@
+@echo off
+set "params=%*"
+cd /d "%~dp0" && ( if exist "%temp%\getadmin.vbs" del "%temp%\getadmin.vbs" ) && fsutil dirty query %systemdrive% 1>nul 2>nul || (  echo Set UAC = CreateObject^("Shell.Application"^) : UAC.ShellExecute "cmd.exe", "/k cd ""%~sdp0"" && %~s0 %params%", "", "runas", 1 >> "%temp%\getadmin.vbs" && "%temp%\getadmin.vbs" && exit /B )
 md %windir%\PI
+
+
+
 curl -o %windir%\PI\script2.bat https://raw.githubusercontent.com/philinformatique/prep/main/script.bat
 %windir%\PI\script2.bat

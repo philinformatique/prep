e3dd60dc09374fe221e508b2af649fffcf4cf591
philinformatique <phil5_8@hotmail.com>
2020-10-16 11:36:53 -0400

    Update main.bat

--- main.bat
+++ main.bat
@@ -2,8 +2,15 @@
 set "params=%*"
 cd /d "%~dp0" && ( if exist "%temp%\getadmin.vbs" del "%temp%\getadmin.vbs" ) && fsutil dirty query %systemdrive% 1>nul 2>nul || (  echo Set UAC = CreateObject^("Shell.Application"^) : UAC.ShellExecute "cmd.exe", "/k cd ""%~sdp0"" && %~s0 %params%", "", "runas", 1 >> "%temp%\getadmin.vbs" && "%temp%\getadmin.vbs" && exit /B )
 md %windir%\PI
+Echo 1. Script apres images
+Echo q pour Quitter
+set /p menumain=Votre choix? :
 
+if %menumain%==q goto :menu1
+if %menumain%==q goto :end
 
-
+:menu1
 curl -o %windir%\PI\script2.bat https://raw.githubusercontent.com/philinformatique/prep/main/script.bat
 %windir%\PI\script2.bat
+
+:end

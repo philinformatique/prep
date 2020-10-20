4cb0f6dea8eb01eb4019d4624bf3f2057b1d633d
philinformatique <phil5_8@hotmail.com>
2020-10-20 10:34:27 -0400

    Update main.bat

--- main.bat
+++ main.bat
@@ -2,7 +2,7 @@
 cls
 set "params=%*"
 cd /d "%~dp0" && ( if exist "%temp%\getadmin.vbs" del "%temp%\getadmin.vbs" ) && fsutil dirty query %systemdrive% 1>nul 2>nul || (  echo Set UAC = CreateObject^("Shell.Application"^) : UAC.ShellExecute "cmd.exe", "/k cd ""%~sdp0"" && %~s0 %params%", "", "runas", 1 >> "%temp%\getadmin.vbs" && "%temp%\getadmin.vbs" && exit /B )
-md %windir%\PI
+if exist %windir%\PI (goto menu1) 
 Echo 1. Script apres images
 Echo q pour Quitter
 set /p menumain=Votre choix? :

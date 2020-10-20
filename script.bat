f0be809a22e1b1739b2a974c74b0511d3711bc35
philinformatique <phil5_8@hotmail.com>
2020-10-20 15:01:58 -0400

    Update script.bat

--- script.bat
+++ script.bat
@@ -1,6 +1,9 @@
 @echo off
 SET mypath=%~dp0
 
+set "params=%*"
+cd /d "%~dp0" && ( if exist "%temp%\getadmin.vbs" del "%temp%\getadmin.vbs" ) && fsutil dirty query %systemdrive% 1>nul 2>nul || (  echo Set UAC = CreateObject^("Shell.Application"^) : UAC.ShellExecute "cmd.exe", "/k cd ""%~sdp0"" && %~s0 %params%", "", "runas", 1 >> "%temp%\getadmin.vbs" && "%temp%\getadmin.vbs" && exit /B )
+
 REG ADD HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Run /v script /t REG_SZ /d C:\Windows\PI\script2.bat /f
 
 cls
